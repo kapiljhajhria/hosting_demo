0ecada31f0e2b93e3fe0702cc444cedd334e068d
kapiljhajhria <jhajhria44@gmail.com>
2020-08-26 11:12:35 +0530

    Updates

---
+++ precache-manifest.59b1181bbef88dc17fefd04ef2f51f36.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad5c889dea7f32d3118ff92589fc7c49",
+    "url": "/*/index.html"
+  },
+  {
+    "revision": "5c3035b311aab2515f1a",
+    "url": "/*/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "60861cd6927aaa7a36fa",
+    "url": "/*/static/css/main.b2b4fe55.chunk.css"
+  },
+  {
+    "revision": "5c3035b311aab2515f1a",
+    "url": "/*/static/js/2.919d906e.chunk.js"
+  },
+  {
+    "revision": "7ab74cd669e480aa80a3619d44d4e67d",
+    "url": "/*/static/js/2.919d906e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "60861cd6927aaa7a36fa",
+    "url": "/*/static/js/main.dc42e198.chunk.js"
+  },
+  {
+    "revision": "3ab73d7123ad8010c484",
+    "url": "/*/static/js/runtime-main.9fde4ead.js"
+  }
+]);
